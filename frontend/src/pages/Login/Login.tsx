8c52e64afdf617bbc6e7d120cc649a726674e9ab
Mari Myrmoen <mari.myr@gmail.com>
2025-11-26 15:05:26 +0100

    feat: Added footer component with logo, text and bamboo decorations

--- frontend/src/pages/Login/Login.tsx
+++ frontend/src/pages/Login/Login.tsx
@@ -1,11 +1,13 @@
 import { Link } from "react-router-dom";
 import "./login.css";
+import Footer from "../../components/footer/Footer";
 
 export default function Login() {
   return (
     <section>
       <h1>Login</h1>
       <Link to={"/register"}>Go to Register</Link>
+      <Footer />
     </section>
   );
 }
