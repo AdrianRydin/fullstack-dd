332d2ee8bd86687edae325f62dd91af09c1fb439
HasanAlshehabi <Hasan.alshehabi99@gmail.com>
2025-11-27 10:19:29 +0100

    bugfixing app.tsx

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -26,6 +26,11 @@ function App() {
         <Route path="/cart" element={<Cart />} />
         <Route path="/about" element={<About />} />
         <Route path="/receipt" element={<Receipt />} />
+        <Route path="/login" element={<Login />} />
+        <Route path="/register" element={<Register />} />
+      </Routes>
+    </BrowserRouter>
+  );
 
 }
 
