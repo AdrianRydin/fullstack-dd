f12cc30be4c0235c3bb9927dddfc6fde52a985cf
HasanAlshehabi <Hasan.alshehabi99@gmail.com>
2025-11-27 09:52:53 +0100

    Added MenuCard comp

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -1,28 +1,7 @@
 import "./App.css";
-import { BrowserRouter, Routes, Route } from "react-router-dom";
-import Home from "./pages/Home/Home";
-import Menu from "./pages/Menu/Menu";
-import Cart from "./pages/Cart/Cart";
-import About from "./pages/About/About";
-import Login from "./pages/Login/Login";
-import Register from "./pages/Register/Register";
-import Receipt from "./pages/Receipt/Receipt";
 
 function App() {
-  return (
-    <BrowserRouter>
-      <Routes>
-        <Route path="/" element={<Home />} />
-        <Route path="/menu" element={<Menu />} />
-        <Route path="/cart" element={<Cart />} />
-        <Route path="/about" element={<About />} />
-        <Route path="/receipt" element={<Receipt />} />
 
-        <Route path="/login" element={<Login />} />
-        <Route path="/register" element={<Register />} />
-      </Routes>
-    </BrowserRouter>
-  );
 }
 
 export default App;
