2ffd47cb42772e37020e09c7228eb15299151ffd
Adrian Rydin <sixxy@pop-os.home>
2025-12-01 11:27:10 +0100

    small fixes

--- frontend/src/components/MenuCard/MenuCard.tsx
+++ frontend/src/components/MenuCard/MenuCard.tsx
@@ -1,4 +1,4 @@
-import "./MenuCard.css";
+import "./menucard.css";
 
 type MenuCardProps = {
   name: string;
@@ -28,7 +28,9 @@ export function MenuCard({
         {onAddToCart && (
           <button className="secondary-button" onClick={onAddToCart}>
             <span className="secondary-button-label">Add to cart</span>
-            <span className="secondary-button-icon"><img src="./src/assets/add-to-cart.png" alt="" /></span>
+            <span className="secondary-button-icon">
+              <img src="./src/assets/add-to-cart.png" alt="" />
+            </span>
           </button>
         )}
       </div>
