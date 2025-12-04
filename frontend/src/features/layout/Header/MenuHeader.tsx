624688af83ff55241b36bd265f671e56bd0b9b86
AdrianRydin <adrian.rydin@gmail.com>
2025-12-04 22:41:31 +0100

    Fixed Header layout for desktop

--- frontend/src/features/layout/Header/MenuHeader.tsx
+++ frontend/src/features/layout/Header/MenuHeader.tsx
@@ -14,26 +14,56 @@ export default function MenuHeader({
 }) {
   return (
     <HeaderLayout>
-      <Link to={"/"}>
-        <ArrowBackIcon
-          className="back-arrow"
-          fontSize="large"
-          sx={{ fontSize: 40, color: "#dfd8c9" }}
-        />
-      </Link>
-
-      <section className="menu-header-cart-container">
-        <Link to={"/cart"}>
-          <ShoppingCartIcon sx={{ fontSize: 30, color: "#dfd8c9" }} />
+      <section className="header-mobile">
+        <Link to={"/"}>
+          <ArrowBackIcon
+            className="back-arrow"
+            fontSize="large"
+            sx={{ fontSize: 40, color: "#dfd8c9" }}
+          />
         </Link>
-        <aside
-          className={`hamburger-menu-container ${isMenuOpen ? "open" : ""}`}
-          onClick={onToggleMenu}
-        >
-          <span className="header-span-1"></span>
-          <span className="header-span-2"></span>
-          <span className="header-span-3"></span>
-        </aside>
+
+        <section className="menu-header-cart-container">
+          <Link to={"/cart"}>
+            <ShoppingCartIcon sx={{ fontSize: 30, color: "#dfd8c9" }} />
+          </Link>
+          <aside
+            className={`hamburger-menu-container ${isMenuOpen ? "open" : ""}`}
+            onClick={onToggleMenu}
+          >
+            <span className="header-span-1"></span>
+            <span className="header-span-2"></span>
+            <span className="header-span-3"></span>
+          </aside>
+        </section>
+      </section>
+
+      <section className="header-desktop">
+        <Link to={"/"}>
+          <ArrowBackIcon
+            className="back-arrow"
+            fontSize="large"
+            sx={{ fontSize: 40, color: "#dfd8c9" }}
+          />
+        </Link>
+
+        <nav className="header-desktop-link-container">
+          <Link to={"/menu"}>
+            <h1>Menu</h1>
+          </Link>
+          <Link to={"/about"}>
+            <h1>About us</h1>
+          </Link>
+          <Link to={"/contact"}>
+            <h1>Contact</h1>
+          </Link>
+          <Link to={"/cart"}>
+            <h1>Cart</h1>
+          </Link>
+          <Link to={"/cart"}>
+            <ShoppingCartIcon sx={{ fontSize: 30, color: "#dfd8c9" }} />
+          </Link>
+        </nav>
       </section>
     </HeaderLayout>
   );
