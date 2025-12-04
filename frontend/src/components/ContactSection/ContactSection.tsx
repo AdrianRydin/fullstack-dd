92a04adfd75b559465f1356406081001d9c82d59
Mari Myrmoen <mari.myr@gmail.com>
2025-12-04 18:54:40 +0100

    Fixed more styling for AboutPage

--- frontend/src/components/ContactSection/ContactSection.tsx
+++ frontend/src/components/ContactSection/ContactSection.tsx
@@ -1,18 +1,26 @@
 import "./contactSection.css";
 import LocalPhoneIcon from "@mui/icons-material/LocalPhone";
 import map from "../../assets/map.png";
+import bamboo from "../../assets/bambo1.png";
 
 function ContactSection() {
   return (
     <section className="contact-section">
       <h1 className="contact-heading">Contact us</h1>
       <section className="contact-layout">
-        <img
-          src={map}
-          alt="Map to find our resturant"
-          className="contact-map"
-        />
-        <section className="contact-info">
+        <article className="contact-left">
+          <img
+            src={map}
+            alt="Map to find our restaurant"
+            className="contact-map"
+          />
+          <img
+            src={bamboo}
+            alt="Map to find our restaurant"
+            className="contact-bamboo"
+          />
+        </article>
+        <article className="contact-right">
           <article className="contact-container">
             <h3 className="contact-subheading">Address</h3>
             <p>Umami Restaurant</p>
@@ -25,14 +33,11 @@ function ContactSection() {
             <p>Sat–Sun: 12:00 – 23:00</p>
           </article>
           <article className="contact-container">
-            <LocalPhoneIcon
-              className="phone-icon"
-              aria-hidden="true"
-            />
+            <LocalPhoneIcon className="phone-icon" aria-hidden="true" />
             <p>Phone: +46 000 000 000</p>
             <p>Email: info@umami.com</p>
           </article>
-        </section>
+        </article>
       </section>
     </section>
   );
