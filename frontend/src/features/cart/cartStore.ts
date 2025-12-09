20c98429a9b5e96c6a60c0959c288956f1772af6
Emilia Myrup <myrupemilia@gmail.com>
2025-12-09 16:29:24 +0100

    changed cart id to string & rearranged admin inventory page

--- frontend/src/features/cart/cartStore.ts
+++ frontend/src/features/cart/cartStore.ts
@@ -4,9 +4,9 @@ import type { CartItem } from "./cartTypes"
 interface CartState {
   items: CartItem[]
   addToCart: (item: CartItem) => void
-  increase: (id: number) => void
-  decrease: (id: number) => void
-  remove: (id: number) => void
+  increase: (id: string) => void
+  decrease: (id: string) => void
+  remove: (id: string) => void
   clear: () => void
   setItems: (items: CartItem[]) => void
 }
@@ -19,31 +19,34 @@ export const useCartStore = create<CartState>((set, get) => ({
   addToCart: (item) => {
     const items = get().items
     const exists = items.find((i) => i.id === item.id)
+
     const newItems = exists
       ? items.map((i) =>
           i.id === item.id ? { ...i, quantity: i.quantity + item.quantity } : i
         )
       : [...items, item]
+
     set({ items: newItems })
   },
 
-  increase: (id: number) => {
+  increase: (id: string) => {
     const newItems = get().items.map((i) =>
       i.id === id ? { ...i, quantity: i.quantity + 1 } : i
     )
     set({ items: newItems })
   },
 
-  decrease: (id: number) => {
+  decrease: (id: string) => {
     const newItems = get()
       .items.map((i) =>
         i.id === id ? { ...i, quantity: Math.max(1, i.quantity - 1) } : i
       )
       .filter((i) => i.quantity > 0)
+
     set({ items: newItems })
   },
 
-  remove: (id: number) => {
+  remove: (id: string) => {
     const newItems = get().items.filter((i) => i.id !== id)
     set({ items: newItems })
   },
--- frontend/src/pages/AdminInventory/AdminInventory.tsx
+++ frontend/src/pages/AdminInventory/AdminInventory.tsx
@@ -7,41 +7,40 @@ function AdminInventoryPage() {
 
       <section className="category-card">
         <h2 className="title">Fish and Seafood</h2>
-        <section className="row-wrapper">
-          <section className="item-wrapper">
-            <h3 className="title">Item</h3>
-            <p>Salmon</p>
-            <p>Salmon</p>
-            <p>Salmon</p>
-            <p>Salmon</p>
-          </section>
-          <section className="qty-wrapper">
-            <h3 className="title">Quantity</h3>
-            <p>2</p>
-            <p>2</p>
-            <p>2</p>
-            <p>2</p>
-          </section>
+        <section className="titles-wrapper">
+          <h3 className="title">Item</h3>
+          <h3 className="title">Quantity</h3>
+        </section>
+        <section className="item-wrapper">
+          {/* här ska vi mappa alla items som finns i backend */}
+          <p>Salmon</p>
+          <p>2</p>
+        </section>
+        <section className="item-wrapper">
+          {/* här ska vi mappa alla items som finns i backend */}
+          <p>Salmon</p>
+          <p>2</p>
+        </section>
+        <section className="item-wrapper">
+          <p>Salmon</p>
+          <p>2</p>
+        </section>
+        <section className="item-wrapper">
+          <p>Salmon</p>
+          <p>2</p>
         </section>
       </section>
 
       <section className="category-card">
         <h2 className="title">Rice and base ingredients</h2>
-        <section className="row-wrapper">
-          <section className="item-wrapper">
-            <h3 className="title">Item</h3>
-            <p>Salmon</p>
-            <p>Salmon</p>
-            <p>Salmon</p>
-            <p>Salmon</p>
-          </section>
-          <section className="qty-wrapper">
-            <h3 className="title">Quantity</h3>
-            <p>2</p>
-            <p>2</p>
-            <p>2</p>
-            <p>2</p>
-          </section>
+        <section className="titles-wrapper">
+          <h3 className="title">Item</h3>
+          <h3 className="title">Quantity</h3>
+        </section>
+        <section className="item-wrapper">
+          {/* här ska vi mappa alla items som finns i backend */}
+          <p>Salmon</p>
+          <p>2</p>
         </section>
       </section>
     </section>
