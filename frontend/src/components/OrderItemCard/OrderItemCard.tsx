1de8c634aa0e4932d02ebb055549d37f43000434
Emilia Myrup <myrupemilia@gmail.com>
2025-12-15 15:54:22 +0100

    fixed type bug in OrderItemCard

--- frontend/src/components/OrderItemCard/OrderItemCard.tsx
+++ frontend/src/components/OrderItemCard/OrderItemCard.tsx
@@ -1,8 +1,8 @@
-import "../CartItemCard/CartItemCard.css";
-import { type SushiRoll } from "../../pages/Cart/Cart";
+import "../CartItemCard/CartItemCard.css"
+import type { CartItem } from "../../features/cart/cartTypes"
 
 interface OrderItemCardProps {
-  item: SushiRoll;
+  item: CartItem
 }
 
 function OrderItemCard({ item }: OrderItemCardProps) {
@@ -18,7 +18,7 @@ function OrderItemCard({ item }: OrderItemCardProps) {
         </article>
       </section>
     </section>
-  );
+  )
 }
 
-export default OrderItemCard;
+export default OrderItemCard
